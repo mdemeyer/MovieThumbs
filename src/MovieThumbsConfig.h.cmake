ec3a7be7b96825597959f90962c0b6777d0cb470
Maarten De Meyer <de.meyer.maarten@gmail.com>
2013-05-25 14:43:46 +0200

    Add version number

---
+++ src/MovieThumbsConfig.h.cmake
@@ -0,0 +1,27 @@
+
+/***************************************************************************
+ *   This file is part of MovieThumbs.                                     *
+ *   Copyright (C) 2013 De Meyer Maarten <de.meyer.maarten@gmail.com>      *
+ *                                                                         *
+ *   MovieThumbs is free software; you can redistribute it and/or modify   *
+ *   it  under the terms of the GNU Lesser General Public License version  *
+ *   2.1 as published by the Free Software Foundation.                     *
+ *                                                                         *
+ *   MovieThumbs is distributed in the hope that it will be useful, but    *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA  02110-1301  USA                                                   *
+ ***************************************************************************/
+
+#ifndef MOVIETHUMBSCONFIG_H
+#define MOVIETHUMBSCONFIG_H
+
+//Version number settings
+#define MOVIETHUMBS_VERSION @MOVIETHUMBS_VERSION@
+
+#endif //MOVIETHUMBSCONFIG_H
