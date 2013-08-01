21c3c73a642333ad5d93656098016eb66ee5bfc3
Maarten De Meyer <de.meyer.maarten@gmail.com>
2013-08-01 16:45:16 +0200

    Port kde plugin to lib

---
+++ kde/moviethumbs.cpp
@@ -0,0 +1,81 @@
+
+/***************************************************************************
+ *   This file is part of MovieThumbs.                                     *
+ *   Copyright (C) 2013 De Meyer Maarten <de.meyer.maarten@gmail.com>      *
+ *                                                                         *
+ *   MovieThumbs is free software; you can redistribute it and/or modify   *
+ *   it  under the terms of the GNU Lesser General Public License version  *
+ *   2.1 as published by the Free Software Foundation.                     *
+ *                                                                         *
+ *   MovieThumbs is distributed in the hope that it will be useful, but    *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA  02110-1301  USA                                                   *
+ ***************************************************************************/
+
+#include "moviethumbs.h"
+#include <movieclient.h>
+
+#include <QtCore/QEventLoop>
+#include <QtCore/QString>
+#include <QtGui/QImage>
+#include <QtNetwork/QNetworkAccessManager>
+
+#include <KDebug>
+#include <solid/networking.h>
+
+QImage thumbnail;
+
+extern "C"
+{
+    KDE_EXPORT ThumbCreator *new_creator()
+    {
+        return new MovieThumbs;
+    }
+}
+
+MovieThumbs::MovieThumbs()
+{
+    m_thumbDownloader = new MovieClient();
+    connect(m_thumbDownloader, SIGNAL(slotPosterFinished(const QImage&)), this, SLOT(setImage(const QImage&)));
+}
+
+MovieThumbs::~MovieThumbs()
+{
+    delete m_thumbDownloader;
+}
+
+bool MovieThumbs::create(const QString &path, int /*w*/, int /*h*/, QImage &img)
+{
+    if(Solid::Networking::status() == Solid::Networking::Unconnected)
+    {
+        kDebug() << "No network connection available";
+        return false;
+    }
+
+    //Reset image
+    thumbnail = QImage();
+
+    //Search and download thumbnail
+    m_thumbDownloader->addSearch(path);
+    img = thumbnail;
+
+    return !img.isNull();
+}
+
+void MovieThumbs::setImage(const QImage &thumb)
+{
+    thumbnail = thumb;
+}
+
+ThumbCreator::Flags MovieThumbs::flags() const
+{
+    return (Flags)(None);
+}
+
+#include "moviethumbs.moc"
---
+++ kde/moviethumbs.h
@@ -0,0 +1,46 @@
+
+/***************************************************************************
+ *   This file is part of MovieThumbs.                                     *
+ *   Copyright (C) 2013 De Meyer Maarten <de.meyer.maarten@gmail.com>      *
+ *                                                                         *
+ *   MovieThumbs is free software; you can redistribute it and/or modify  *
+ *   it  under the terms of the GNU Lesser General Public License version  *
+ *   2.1 as published by the Free Software Foundation.                     *
+ *                                                                         *
+ *   MovieThumbs is distributed in the hope that it will be useful, but   *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,            *
+ *   MA  02110-1301  USA                                                   *
+ ***************************************************************************/
+
+#ifndef MOVIETHUMBS_H
+#define MOVIETHUMBS_H
+
+#include <QtCore/QObject>
+#include <QImage>
+
+#include <kio/thumbcreator.h>
+#include <movieclient.h>
+
+class MovieThumbs : public QObject, public ThumbCreator
+{
+    Q_OBJECT
+
+public:
+    MovieThumbs();
+    virtual ~MovieThumbs();
+    virtual bool create(const QString& path, int width, int height, QImage& img);
+    virtual Flags flags() const;
+
+    MovieClient *m_thumbDownloader;
+
+public slots:
+    void setImage(const QImage &);
+};
+
+#endif // MOVIETHUMBS_H
